a311d15ef80a13447cc9c984b2415c76d8bb31db
murase <murase@172.20.10.7>
2016-10-27 20:42:18 +0900

    commit

---
+++ src/hoge/testttttt.java
@@ -0,0 +1,10 @@
+package hoge;
+
+public class testttttt {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Yahooooooooooooooooooo");
+	}
+
+}
